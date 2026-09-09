d769eacec1407ceb4512fcfa02ec0b5b8a2ae143
Xandercrash1 <135729075+Xandercrash1@users.noreply.github.com>
2026-09-09 13:45:31 -0400

    fix: change formatting toolbar from mouse-tracking to a reliable fixed top-center anchor

--- public/components/DocBodyWidget.js
+++ public/components/DocBodyWidget.js
@@ -280,18 +280,9 @@ class DocBodyWidget extends HTMLElement {
         // Text is selected!
         toolbar.classList.remove('hidden');
         
-        // Position it at mouse if available, else center top
-        if (e && e.clientX) {
-          const rect = this.getBoundingClientRect();
-          let topPos = e.clientY - rect.top;
-          if (topPos < 50) topPos = 50; // Prevent clipping at the very top of the widget
-          
-          toolbar.style.left = (e.clientX - rect.left) + 'px';
-          toolbar.style.top = topPos + 'px';
-        } else {
-          toolbar.style.left = '50%';
-          toolbar.style.top = '60px';
-        }
+        // Anchor it reliably to the top center of the editor
+        toolbar.style.left = '50%';
+        toolbar.style.top = '60px';
       } else {
         toolbar.classList.add('hidden');
       }
